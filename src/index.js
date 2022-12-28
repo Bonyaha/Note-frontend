3c7651ad3187cdda844d626c5d029f6e804f165b
Roman <romchikus1987@gmail.com>
2022-12-28 19:21:44 +0200

    changed path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.css';
-import App from './countries/app';
+import App from './part 2/App';
 
 /* const notes = [
   {
