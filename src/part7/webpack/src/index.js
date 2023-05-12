6fc3871da505a40ec35226b8663632d65c7c0c15
Roman <romchikus1987@gmail.com>
2023-05-12 16:30:58 +0300

    updates

---
+++ src/part7/webpack/src/index.js
@@ -0,0 +1,13 @@
+import 'core-js/stable/index.js'
+import 'regenerator-runtime/runtime.js'
+import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import App from './App'
+
+const hello = (name) => {
+  console.log(`hello ${name}`)
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
